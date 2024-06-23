b8f95e0be3b44d343a5a1d2fc0b21760a93e25a8
Titty <mehran9326@gmail.com>
2024-06-23 14:30:22 +0330

    header is finished and responsived

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+const hamburgerIcon = document.getElementById("hamburger-icon"),
+  crossIcon = document.getElementById("cross-icon"),
+  mobileMenu = document.getElementById("mobile-menu"),
+  mobileMenuFade = document.getElementById("mobile-menu-fade");
+
+hamburgerIcon.addEventListener("click", () => {
+  mobileMenu.classList.add("show-mobile-menu");
+  mobileMenuFade.classList.add("show-mobile-menu");
+});
+
+crossIcon.addEventListener("click", () => {
+  mobileMenu.classList.remove("show-mobile-menu");
+  mobileMenuFade.classList.remove("show-mobile-menu");
+});
